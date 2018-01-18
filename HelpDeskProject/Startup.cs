a277c5518b57a5facd39bf4f8fa641073f1dbea4
Hope <shafieihope@outlook.com>
2018-01-18 15:37:38 +0330

    Add project files.

---
+++ HelpDeskProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelpDeskProject.Startup))]
+namespace HelpDeskProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

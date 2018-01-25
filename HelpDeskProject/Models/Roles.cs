eb4d41f86b0898b15f39d7145be2ac17574bac8e
Hope <shafieihope@outlook.com>
2018-01-25 14:48:26 +0330

    before creating roles and making some stupid desc..

---
+++ HelpDeskProject/Models/Roles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpDeskProject.Models
+{
+    public class Roles
+    {
+    }
+}
--- HelpDeskProject/Startup.cs
+++ HelpDeskProject/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
 using Owin;
+using HelpDeskProject.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 [assembly: OwinStartupAttribute(typeof(HelpDeskProject.Startup))]
 namespace HelpDeskProject
@@ -10,5 +13,72 @@ namespace HelpDeskProject
         {
             ConfigureAuth(app);
         }
+        // Create User and Roles 
+
+        private void CreateRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            // In Startup iam creating first Admin Role and creating a default Admin User    
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool   
+                var role = new IdentityRole
+                {
+                    Name = "Admin"
+                };
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                  
+
+                var user = new ApplicationUser
+                {
+                    UserName = "Hope",
+                    Email = "shafieihope@outlook.com"
+                };
+
+                string userPWD = "asd123ASD!@#";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin   
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+
+                }
+            }
+
+            // creating Creating Manager role    
+            if (!roleManager.RoleExists("Manager"))
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Manager"
+                };
+                roleManager.Create(role);
+
+            }
+
+            // creating Creating Employee role    
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole
+                {
+                    Name = "Employee"
+                };
+                roleManager.Create(role);
+
+            }
+        }
+
+
+
     }
+
 }
